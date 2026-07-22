b549a2c465b245c745c3e0b6b10056bc877bffdb
Hakim <shanun036@gmail.com>
2026-07-22 14:21:34 +0800

    feat: add PopunderAd component with ad script integration

--- app/components/PopunderAd.js
+++ app/components/PopunderAd.js
@@ -4,10 +4,27 @@ import Script from 'next/script';
 
 export default function PopunderAd() {
   return (
-    <Script
-      id="popunder-ad"
-      src="/ads/popunder.js"
-      strategy="afterInteractive"
-    />
+    <>
+      <Script
+        id="popunder-ad"
+        src="/ads/popunder.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+          try {
+            if (typeof pop !== 'undefined' && pop.popMethods) {
+              pop.popMethods
+                .add('https://fluffy-machine.com/bh3.Vm0OP/3jpSvzbNmmVLJ/ZZDi0s3vMsj/Qm2zO/T-YIxaLpTzc/ybNDDXY_5iNCjmUp', {
+                  under: true,
+                  newTab: true,
+                  cookieExpires: 86400,
+                })
+                .config({
+                  perpage: 1,
+                });
+            }
+          } catch (e) {}
+        }}
+      />
+    </>
   );
 }
